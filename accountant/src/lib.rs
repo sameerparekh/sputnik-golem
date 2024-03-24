8b130dd246a30dd8e74c357b7ee98939e49f6fcc
Sameer Brenn <sameer@creativedestruction.com>
2024-03-24 15:50:07 -0600

    fix compile

--- accountant/src/lib.rs
+++ accountant/src/lib.rs
@@ -9,6 +9,7 @@ use crate::bindings::exports::sputnik::accountant::api::Error::{
 };
 use crate::bindings::golem::rpc::types::RpcError::RemoteInternalError;
 use crate::bindings::golem::rpc::types::Uri;
+use crate::bindings::sputnik::registry::api::Asset;
 use crate::bindings::sputnik::registry_stub::stub_registry;
 
 mod bindings;
@@ -39,14 +40,14 @@ fn get_registry() -> stub_registry::Api {
     stub_registry::Api::new(&uri)
 }
 
-// TODO: Actually calculate available balance given orders
-
 const ZERO: u64 = 0u64;
-fn available_balance(asset_id: u64) -> u64 {
-    with_state(|state| match state.balances.get(&asset_id) {
+
+// TODO: Actually calculate available balance given orders
+fn available_balance(state: &mut State, asset_id: u64) -> u64 {
+    match state.balances.get(&asset_id) {
         Some(balance) => *balance,
         None => ZERO,
-    })
+    }
 }
 
 impl Guest for Component {
@@ -68,13 +69,16 @@ impl Guest for Component {
                 .iter()
                 .filter_map(|(asset_id, balance)| {
                     match assets.iter().find(|asset| asset.id == *asset_id) {
-                        Some(asset) => Some(AssetBalance {
-                            asset: asset.clone(),
-                            balance: *balance,
-                            available_balance: available_balance(*asset_id),
-                        }),
+                        Some(asset) => Some((asset.clone(), *balance)),
                         None => None,
                     }
+                })
+                .collect::<Vec<(Asset, u64)>>()
+                .iter()
+                .map(|(asset, balance)| AssetBalance {
+                    asset: asset.clone(),
+                    balance: *balance,
+                    available_balance: available_balance(state, asset.id.clone()),
                 })
                 .collect()
         })
@@ -93,7 +97,7 @@ impl Guest for Component {
                 Some(asset) => AssetBalance {
                     asset: asset.clone(),
                     balance: state.balances.get(&asset_id).unwrap().clone(),
-                    available_balance: available_balance(asset_id),
+                    available_balance: available_balance(state, asset_id),
                 },
                 None => panic!("No asset_id {}", asset_id),
             }
@@ -102,11 +106,12 @@ impl Guest for Component {
 
     fn withdraw(asset_id: u64, amount: u64) -> Result<AssetBalance, Error> {
         let assets = get_registry().get_assets();
-        let available = available_balance(asset_id);
-        if available <= amount {
-            Err(InsufficientFunds(available))
-        } else {
-            with_state(|state| {
+
+        with_state(|state| {
+            let available = available_balance(state, asset_id);
+            if available <= amount {
+                Err(InsufficientFunds(available))
+            } else {
                 state
                     .balances
                     .entry(asset_id)
@@ -115,11 +120,11 @@ impl Guest for Component {
                     Some(asset) => Ok(AssetBalance {
                         asset: asset.clone(),
                         balance: state.balances.get(&asset_id).unwrap().clone(),
-                        available_balance: available_balance(asset_id),
+                        available_balance: available_balance(state, asset_id),
                     }),
                     None => panic!("No asset_id {}", asset_id),
                 }
-            })
-        }
+            }
+        })
     }
 }
