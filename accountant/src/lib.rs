b368f4007788fff1c438c41b70c64c717f79f460
Sameer Brenn <sameer@creativedestruction.com>
2024-03-23 21:40:51 -0600

    add get balances fn

--- accountant/src/lib.rs
+++ accountant/src/lib.rs
@@ -28,6 +28,15 @@ fn with_state<T>(f: impl FnOnce(&mut State) -> T) -> T {
     STATE.with_borrow_mut(f)
 }
 
+static REGISTRY: stub_registry::Api = {
+    let template_id = std::env::var("REGISTRY_TEMPLATE_ID").expect("REGISTRY_TEMPLATE_ID not set");
+    let uri = Uri {
+        value: format!("worker://{template_id}/{}", "registry"),
+    };
+
+    stub_registry::Api::new(&uri)
+};
+
 impl Guest for Component {
     fn initialize(id: u64) -> Result<u64, Error> {
         with_state(|state| match state.id {
@@ -40,16 +49,23 @@ impl Guest for Component {
     }
 
     fn get_balances() -> Vec<AssetBalance> {
-        let template_id =
-            std::env::var("REGISTRY_TEMPLATE_ID").expect("REGISTRY_TEMPLATE_ID not set");
-        let uri = Uri {
-            value: format!("worker://{template_id}/{}", "blah"),
-        };
-
-        // Connecting to the remote worker and invoking it
-        let registry = stub_registry::Api::new(&uri);
-        registry.get_spot_pairs();
-        vec![]
+        let assets = REGISTRY.get_assets();
+        with_state(|state| {
+            state
+                .balances
+                .iter()
+                .filter_map(|(asset_id, balance)| {
+                    match assets.iter().find(|asset| asset.id == *asset_id) {
+                        Some(asset) => Some(AssetBalance {
+                            asset: asset.clone(),
+                            balance: *balance,
+                            available_balance: *balance,
+                        }),
+                        None => None,
+                    }
+                })
+                .collect()
+        })
     }
 
     fn place_order(order: Order) -> Result<OrderStatus, Error> {
