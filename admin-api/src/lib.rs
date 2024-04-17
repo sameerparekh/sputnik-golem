42a4f028b811561236b900285471666a82f97d74
Sameer Brenn <sameer@creativedestruction.com>
2024-04-17 09:30:26 -0600

    basic code for admin api

--- admin-api/src/lib.rs
+++ admin-api/src/lib.rs
@@ -1,11 +1,17 @@
 use std::cell::RefCell;
+use std::env;
 
 use mockall::automock;
-// use reqwest::blocking::Client;
+use reqwest::Client;
 use serde::{Deserialize, Serialize};
 
-use crate::bindings::exports::sputnik::admin_api::api::Guest;
-use crate::bindings::sputnik::registry::api::{Asset, HydratedSpotPair, SpotPair};
+use crate::bindings::exports::sputnik::admin_api::api::{Error, Guest};
+use crate::bindings::exports::sputnik::admin_api::api::Error::{Internal, UnableToMakeEngine};
+use crate::bindings::golem::rpc::types::Uri;
+use crate::bindings::sputnik::ids_stub::stub_ids;
+use crate::bindings::sputnik::registry::api::{
+    Asset, Error as RegistryError, HydratedSpotPair, SpotPair,
+};
 use crate::bindings::sputnik::registry_stub::stub_registry;
 
 mod bindings;
@@ -13,11 +19,11 @@ mod bindings;
 struct Component;
 
 struct State {
-    last_id: u64,
+    external_service_api: Box<dyn ExternalServiceApi>,
 }
 
 thread_local! {
-    static STATE: RefCell<State> = RefCell::new(State { last_id: 0u64 });
+    static STATE: RefCell<State> = RefCell::new(State { external_service_api: Box::new(ExternalServiceApiProd) });
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -36,40 +42,111 @@ impl CreateWorkerBody {
         }
     }
 }
-// fn create_matching_engine(spot_pair_id: u64) {
-//     let client = Client::new();
-//     let template_id = env::var("MATCHING_ENGINE_TEMPLATE_ID").unwrap();
-//     let url = format!(
-//         "https://release.api.golem.cloud/v1/templates/{}/workers",
-//         template_id
-//     );
-//     let body = CreateWorkerBody::new(format!("{}", spot_pair_id));
-//     let token = env::var("GOLEM_TOKEN_SECRET").unwrap();
-//     let response = client
-//         .post(url)
-//         .json(&body)
-//         .header("Authorization", format!("Bearer {}", token))
-//         .send()
-//         .unwrap();
-//     assert!(response.status().is_success());
-// }
 
 #[automock]
 trait ExternalServiceApi {
-    // fn get_registry(&self) -> stub_registry::Api;
-    // fn get_ids(&self) -> stub_ids::Api;
+    fn get_registry(&self) -> stub_registry::Api;
+    fn get_ids(&self) -> stub_ids::Api;
+
+    fn get_new_id(&self) -> u64;
+
+    fn create_asset(&self, asset: &Asset) -> Result<Asset, RegistryError>;
+
+    fn create_spot_pair(&self, spot_pair: &SpotPair) -> Result<HydratedSpotPair, RegistryError>;
+
+    fn create_matching_engine(&self, spot_pair_id: u64) -> Result<(), Error>;
+}
+
+pub struct ExternalServiceApiProd;
+
+impl ExternalServiceApi for ExternalServiceApiProd {
+    fn get_registry(&self) -> stub_registry::Api {
+        let template_id = env::var("REGISTRY_TEMPLATE_ID").expect("REGISTRY_TEMPLATE_ID not set");
+        let uri = Uri {
+            value: format!("worker://{template_id}/{}", "registry"),
+        };
+
+        stub_registry::Api::new(&uri)
+    }
+
+    fn get_ids(&self) -> stub_ids::Api {
+        let template_id = env::var("IDS_TEMPLATE_ID").expect("IDS_TEMPLATE_ID not set");
+        let uri = Uri {
+            value: format!("worker://{template_id}/{}", "ids"),
+        };
+
+        stub_ids::Api::new(&uri)
+    }
+
+    fn get_new_id(&self) -> u64 {
+        self.get_ids().get_new_id()
+    }
+
+    fn create_asset(&self, asset: &Asset) -> Result<Asset, RegistryError> {
+        self.get_registry().add_asset(asset)
+    }
+
+    fn create_spot_pair(&self, spot_pair: &SpotPair) -> Result<HydratedSpotPair, RegistryError> {
+        self.get_registry().add_spot_pair(spot_pair)
+    }
+    fn create_matching_engine(&self, spot_pair_id: u64) -> Result<(), Error> {
+        let client = Client::new();
+        let template_id =
+            env::var("MATCHING_ENGINE_TEMPLATE_ID").expect("MATCHING_ENGINE_TEMPLATE_ID not set");
+        let url = format!(
+            "https://release.api.golem.cloud/v1/templates/{}/workers",
+            template_id
+        );
+        let body = CreateWorkerBody::new(format!("{}", spot_pair_id));
+        let token = env::var("GOLEM_TOKEN_SECRET").expect("GOLEM_TOKEN_SECRET not set");
+        match client
+            .post(url)
+            .json(&body)
+            .header("Authorization", format!("Bearer {}", token))
+            .send()
+        {
+            Ok(_) => Ok(()),
+            Err(err) => Err(UnableToMakeEngine(format!("{}", err))),
+        }
+    }
 }
 
 fn with_state<T>(f: impl FnOnce(&mut State) -> T) -> T {
     STATE.with_borrow_mut(f)
 }
 impl Guest for Component {
-    fn create_asset() -> u64 {
-        todo!()
+    fn create_asset(name: String, decimals: u8) -> Result<Asset, Error> {
+        with_state(|state| {
+            let asset_id = state.external_service_api.get_new_id();
+            match state.external_service_api.create_asset(&Asset {
+                id: asset_id,
+                name,
+                decimals,
+            }) {
+                Ok(result) => Ok(result),
+                Err(err) => Err(Internal(format!("{}", err))),
+            }
+        })
     }
 
-    fn create_spot_pair() -> u64 {
-        todo!()
+    fn create_spot_pair(
+        name: String,
+        numerator: u64,
+        denominator: u64,
+    ) -> Result<HydratedSpotPair, Error> {
+        with_state(|state| {
+            let pair_id = state.external_service_api.get_new_id();
+            state.external_service_api.create_matching_engine(pair_id)?;
+            match state.external_service_api.create_spot_pair(&SpotPair {
+                id: pair_id,
+                name,
+                numerator_id: numerator,
+                denominator_id: denominator,
+            }) {
+                Ok(result) => Ok(result),
+                Err(err) => Err(Internal(format!("{}", err))),
+            }
+        })
     }
 }
 
