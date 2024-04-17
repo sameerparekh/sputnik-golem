0364d784cafd549a8e0eb6ca2c1265f6951892d4
Sameer Brenn <sameer@creativedestruction.com>
2024-04-17 14:15:44 -0600

    script supports cloud and local

--- scripts/update-template.sh
+++ scripts/update-template.sh
@@ -1,4 +1,39 @@
 #!/usr/bin/env bash
-TEMPLATE_NAME=$1
+usage() {
+  echo "Usage: $0 [-c] template-name"
+  echo "  -c           : Use golem cloud"
+  echo "  template-name: Template to update"
+}
 
-golem-cloud-cli template update --template-name "${TEMPLATE_NAME}" target/wasm32-wasi/debug/"${TEMPLATE_NAME}".wasm
+USE_CLOUD=false
+
+while getopts ":c" opt; do
+  case $opt in
+    c)
+      USE_CLOUD=true
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+shift $((OPTIND - 1))
+
+if [ "$#" -ne 1 ]; then
+  usage
+  exit 1
+fi
+
+TEMPLATE_NAME="$1"
+
+if [ "$USE_CLOUD" = 'true' ]; then
+  CMD=golem-cloud-cli
+else
+  CMD=golem-cli
+fi
+
+TEMPLATE_NAME_W_UNDERSCORE=$(echo "$TEMPLATE_NAME" | tr '-' '_')
+
+"$CMD" template update --template-name "${TEMPLATE_NAME}" target/wasm32-wasi/debug/"${TEMPLATE_NAME_W_UNDERSCORE}".wasm
--- scripts/update-templates.sh
+++ scripts/update-templates.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
+usage() {
+  echo "Usage: $0 [-c]"
+  echo "  -c           : Use golem cloud"
+}
 
-scripts/update-template.sh ids
-scripts/update-template.sh registry
-scripts/update-template.sh adminapi
-scripts/update-template.sh accountant
-scripts/update-template.sh matching-engine
+scripts/update-template.sh "$@" ids
+scripts/update-template.sh "$@" registry
+scripts/update-template.sh "$@" adminapi
+scripts/update-template.sh "$@" accountant
+scripts/update-template.sh "$@" matching-engine
